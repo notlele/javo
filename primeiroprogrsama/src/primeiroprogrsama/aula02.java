2d5999de1030840b1bf6df9792f038956a62ddae
Letícia Souza <38688289+fairykei@users.noreply.github.com>
2019-02-18 19:17:36 -0300

    aula02

--- primeiroprogrsama/src/primeiroprogrsama/aula02.java
+++ primeiroprogrsama/src/primeiroprogrsama/aula02.java
@@ -5,11 +5,16 @@
  */
 package primeiroprogrsama;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Aluno
  */
 public class aula02 {
+    
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner (System.in);
     /*
         System.out.println("Quantos amigos vão rachar a pizza? ");
         int amigos = scaner.nextInt();
@@ -18,8 +23,63 @@ public class aula02 {
         sabor = scaner.nextLine();
         System.out.println("Qual o valor da pizza? ");
         float vlr = scaner.nextInt();
+        System.out.println("Qual o valor máximo para rachar? ");
+        float racharmax = scaner.nextInt();
         
         float rachar = vlr / amigos;
-        System.out.println("A pizza de " + sabor + " custa R$" + vlr + " e cada amigo vai pagar R$" + rachar);
+        float totalmax = racharmax * amigos;
+        
+        if (rachar <= racharmax) {
+            System.out.println("A pizza de " + sabor + " custa R$" + vlr + " e cada amigo vai pagar R$" + rachar);
+        }
+        else {
+            System.out.println("Deu ruim no racha. A pizza deveria custar no máximo R$" + totalmax);
+        }
+        
+        // -----------------------------------------------------------
+        
+        System.out.println("Qual o nome do responsável pela família?");
+        String responsavel = scaner.nextLine();
+        System.out.println("Qual a cidade de residência?");
+        String cidade = scaner.nextLine();
+        System.out.println("Há quantos filhos menores de 18?");
+        int filhos = scaner.nextInt();
+        System.out.println("Há quantos filhos PNE?");
+        int pne = scaner.nextInt();
+        System.out.println("Há quantos idosos (60+)?");
+        int idosos = scaner.nextInt();
+        
+        double vlr18 = filhos * 12.50;
+        int vlrpne = pne * 14;
+        int vlridoso = idosos * 10;
+        double total = vlr18 + vlrpne + vlridoso;
+        
+        
+        System.out.println("A família do responsável " + responsavel + " vai receber um total de R$" + total + " por mês \n- Valor pelos menores de 18 anos: R$"
+        + vlr18 + "\n- Valor pelos PNE: R$" + vlrpne + "\n- Valor pelos idosos: R$" + vlridoso + "\n - Cidade: " + cidade);
+    
+        // -----------------------------------------------------------
+        
+        System.out.println("Insira o 1º nome: ");
+        String nome1 = scaner.nextLine();
+        System.out.println("Insira a altura do 1º: ");
+        double altura1 = scaner.nextInt();
+        System.out.println("Insira o 2º nome: ");
+        String nome2 = scaner.nextLine();
+        System.out.println("Insira a altura do 2º: ");
+        double altura2 = scaner.nextInt();
+        
+        if (altura1 > altura2) {
+            System.out.println(nome1 + " é mais alto que " + nome2 + "(" + altura1 + " x " + altura2 + ")");
+        }
+        else if (altura2 > altura1) {
+            System.out.println(nome2 + " é mais alto que " + nome1 + "(" + altura2 + " x " + altura1 + ")");
+        }
+        else {
+            System.out.println("Os dois têm a mesma altura.");   
+        }
     */
+    
+    
+    }
 }
