cfe664cca50d551e9af59dc91c18cfcbb90711a5
Letícia Souza <38688289+fairykei@users.noreply.github.com>
2019-08-07 21:12:01 -0300

    o bli ga do

---
+++ TestaFigura/src/Figura.java
@@ -0,0 +1,42 @@
+public abstract class Figura {
+    private String cor;
+    private String tipo;
+    private double altura;
+    private double base;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double calculaArea() {
+        return base * altura;
+    }
+}
---
+++ TestaFigura/src/TestaFigura.java
@@ -0,0 +1,5 @@
+public class TestaFigura {
+    public static void main(String[] args) {
+        Triangulo triangulo = new Triangulo();
+    }
+}
